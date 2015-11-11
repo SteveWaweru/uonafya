1c299a145af64669e7df22c66f54081fab5017b9
steve <steviewez@gmail.com>
2015-11-11 10:53:37 +0300

    created landing page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render_to_response('dashboard/index.html')
